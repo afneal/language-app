c30cb95ffd52d41b0d622b6cdece1d4f99c237cc
afneal <Afneal0226@gmail.com>
2026-04-29 16:52:33 -0500

    Initial Setup

---
+++ backend/src/main/java/io/github/afneal/language_app_backend/LanguageAppBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.afneal.language_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageAppBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/afneal/language_app_backend/LanguageAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.afneal.language_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
